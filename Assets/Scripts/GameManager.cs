4f5485fea8f919dd59d95271f4b78af67c657eb6
Keaton White <kenkyo@gmail.com>
2018-03-15 17:05:46 -0500

    Adding UIManager script; added OnGamePause delegate to GameEvents; added initialization of GameEvent delegates to GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+	private void Awake()
+	{
+        GameEvents.Initialise();
+	}
+
 	// Use this for initialization
 	void Start () {
 		
