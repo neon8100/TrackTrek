abc11918ffb458242eec7e8ec29894cef6d0e430
Keaton White <kenkyo@gmail.com>
2018-03-15 15:57:50 -0500

    MasterCanvas in UI scene detects player pressing a button and turns on/off pause panel in response

---
+++ Assets/Scripts/PressToPause.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PressToPause : MonoBehaviour {
+
+    private bool pressedPauseButton;
+    [SerializeField]
+    private GameObject pausePanel;
+
+	// Use this for initialization
+	void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(pressedPauseButton){
+            PauseGame();
+            pressedPauseButton = false;
+        }
+	}
+
+	private void FixedUpdate()
+	{
+        if (CrossPlatformInputManager.GetButtonDown("Fire1"))
+        {
+            pressedPauseButton = true;
+        }
+	}
+
+    //Currently only activates the Pause Panel
+    private void PauseGame(){
+        if(!pausePanel.activeInHierarchy){
+            pausePanel.SetActive(true);
+        } else if (pausePanel.activeInHierarchy){
+            pausePanel.SetActive(false);
+        }
+
+    }
+
+}
