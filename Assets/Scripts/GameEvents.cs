23f3646a835336b1120af3019f13a7ab301047c3
Matt Gambell <matt@gsproductions.co.uk>
2018-03-15 19:52:56 +0000

    Created tilegen scene and started gameevents class.

---
+++ Assets/Scripts/GameEvents.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEvents
+{
+    public static GameEvents events;
+    /// <summary>
+    /// Initialised the GameEvent static member. Call this at the start if the app.
+    /// </summary>
+    public static void Initialise()
+    {
+        events = new GameEvents();
+        events.Init();
+    }
+
+    //Decalere a new delegate type
+    public delegate void OnGameStart();
+    public delegate void OnGameOver();
+
+    //Add a variable of the delegate type to be listened to when calling events.
+    public OnGameStart onGameStart;
+    public OnGameOver onGameOver;
+
+    /// <summary>
+    /// Initialised the GameEvent delegates. Call this in the GameManager when a new game starts.
+    /// </summary>
+    public void Init()
+    {
+        events = this;
+    }
+
+    /// <summary>
+    /// Resets all the GameEvent delegates. Call this to reset the GameEvents.
+    /// </summary>
+    public void ClearAll()
+    {
+        
+        onGameStart = null;
+        onGameOver = null;
+    }
+
+
+}
