91106053eaba25d7c4f957b775cd81e51145ac85
Matt Gambell <matt@gsproductions.co.uk>
2018-03-15 22:55:42 -0500

    Added pickup mechanic.

---
+++ Assets/CollectableMaterial.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableMaterial : MonoBehaviour
+{
+    Rigidbody2D body;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    GameObject parent;
+
+    public void Hold(GameObject character)
+    {
+
+        body.simulated = false;
+        parent = character;
+    }
+
+    public void Drop()
+    {
+        body.simulated = true;
+        parent = null;
+    }
+
+    public void Update()
+    {
+        if (parent == null) { return; }
+        else
+        {
+            transform.position = parent.transform.position;
+        }
+    }
+
+}
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public enum PlayerID
 {
@@ -11,6 +12,7 @@ public enum PlayerID
 public class PlayerController : MonoBehaviour {
 
     public PlayerID playerNumber;
+    int playerId;
     public float lookAheadSpeed;
     public float playerSpeed;
 
@@ -21,17 +23,27 @@ public class PlayerController : MonoBehaviour {
         GameObject t = new GameObject();
         lookTarget = t.transform;
 
-        foreach(string s in Input.GetJoystickNames())
+        playerId = (int)playerNumber + 1;
+
+        foreach (string s in Input.GetJoystickNames())
         {
             Debug.Log(s);
         }
+
+
+        onActionButtonDown = new UnityEvent();
+        onActionButtonUp = new UnityEvent();
+        onActionButtonStay = new UnityEvent();
+
+        onActionButtonDown.AddListener(CheckActionInputs);
+
+
 
     }
 
     private void Update()
     {
-        int playerId = (int)playerNumber;
-
+        
         float vertical = Input.GetAxis("VerticalP"+playerId);
         float horizontal = Input.GetAxis("HorizontalP"+playerId);
 
@@ -43,5 +55,92 @@ public class PlayerController : MonoBehaviour {
         transform.up = lookTarget.position;
         transform.position = Vector3.Lerp(transform.position, lookTarget.position, playerSpeed);
 
+        
     }
+
+    private void LateUpdate()
+    {
+        //Button Event Listeners
+        if (actionButton > 0)
+        {
+            if (!actionButtonIsDown)
+            {
+                onActionButtonDown.Invoke();
+            }
+
+            onActionButtonStay.Invoke();
+
+            actionButtonIsDown = true;
+        }
+        else
+        {
+            onActionButtonUp.Invoke();
+            actionButtonIsDown = false;
+        }
+    }
+
+
+    //Checks collisions and then determines what to try and do
+    public void OnCollisionStay2D(Collision2D collision)
+    {
+        this.collision = collision;
+    }
+
+    public void OnCollisionExit2D(Collision2D collision)
+    {
+        this.collision = null;
+    }
+
+    bool holdingMaterial;
+
+    public Collision2D collision;
+    public CollectableMaterial material;
+
+    void HandleMaterialCollision(CollectableMaterial material)
+    {
+        if (actionButton > 0 && this.material!=material)
+        {
+            material.Hold(gameObject);
+            this.material = material;
+            holdingMaterial = true;
+            
+        }
+    }
+
+    void CheckActionInputs()
+    {
+        if (holdingMaterial)
+        {
+            material.Drop();
+            holdingMaterial = false;
+            this.material = null;
+        }
+        else
+        {
+            if (collision.gameObject.GetComponent<CollectableMaterial>())
+            {
+                //Handles a collision against a material object
+                HandleMaterialCollision(collision.gameObject.GetComponent<CollectableMaterial>());
+            }
+        }
+    }
+
+    /*
+    void CheckButtonPresses()
+    {
+        if (actionButton>0 && !actionButtonIsDown)
+        {
+            onActionButtonDown.Invoke();
+            
+        }
+    }
+    */
+
+    float actionButton { get { return Input.GetAxis("FireP" + playerId); } }
+
+    bool actionButtonIsDown;
+
+    UnityEvent onActionButtonDown;
+    UnityEvent onActionButtonStay;
+    UnityEvent onActionButtonUp;
 }
