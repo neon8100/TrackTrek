eee3342766c45407dc010bfdca6a79ad5fbf72cb
Matt Gambell <matt@gsproductions.co.uk>
2018-03-15 21:49:06 -0500

    Added basic player controler.

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerID
+{
+    Player1,
+    Player2
+}
+
+public class PlayerController : MonoBehaviour {
+
+    public PlayerID playerNumber;
+    public float lookAheadSpeed;
+    public float playerSpeed;
+
+    Transform lookTarget;
+
+    private void Awake()
+    {
+        GameObject t = new GameObject();
+        lookTarget = t.transform;
+
+        foreach(string s in Input.GetJoystickNames())
+        {
+            Debug.Log(s);
+        }
+
+    }
+
+    private void Update()
+    {
+        int playerId = (int)playerNumber;
+
+        float vertical = Input.GetAxis("VerticalP"+playerId);
+        float horizontal = Input.GetAxis("HorizontalP"+playerId);
+
+        Vector2 pos = new Vector2(lookTarget.position.x, lookTarget.position.y);
+        Vector2 newPos = pos + new Vector2(horizontal * lookAheadSpeed, vertical * lookAheadSpeed);
+
+        lookTarget.position = newPos;
+
+        transform.up = lookTarget.position;
+        transform.position = Vector3.Lerp(transform.position, lookTarget.position, playerSpeed);
+
+    }
+}
