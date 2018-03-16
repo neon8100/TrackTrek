2176fcfe2bdf7f4e1cfc0b523af6f3fdf28d9be9
Keaton White <kenkyo@gmail.com>
2018-03-15 23:33:31 -0500

    UI Scene is now loaded into game by GameManager; UIScene and UIManager now reflect that; still some bugs with UI structure

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
-    private bool isGamePaused = false;
+    private bool isGamePaused;
+
+    public bool IsGamePaused { get; private set; } 
 
 	private void Awake()
 	{
         GameEvents.Initialise();
+        LoadUIManagerScene();
+
+        if(gameObject.tag != "GameManager"){
+            gameObject.tag = "GameManager";
+        }
 	}
 
 	// Use this for initialization
@@ -19,10 +28,16 @@ public class GameManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         print(Time.timeScale);
+        print("Game is paused: "+isGamePaused);
 	}
 
-    public void StartGame(){
-        return;
+	private void FixedUpdate()
+	{
+       
+	}
+
+	public void StartGame(){
+        
     }
 
     public void ResetGame(){
@@ -35,12 +50,22 @@ public class GameManager : MonoBehaviour {
     }
 
     public void SetTimeScale(){
-        if(!isGamePaused){
+        if (!isGamePaused) { isGamePaused = true; }
+        else if (isGamePaused) { isGamePaused = false; }
+
+        if(isGamePaused){
             Time.timeScale = 0f;
-            isGamePaused = true; 
+            print("Stopped time");
         } else {
             Time.timeScale = 1f;
-            isGamePaused = false;
+            print("Unstopped time");
+        }
+    }
+
+    private void LoadUIManagerScene(){
+        if (!SceneManager.GetSceneByName("UIScene").isLoaded)
+        {
+            SceneManager.LoadScene("UIScene", LoadSceneMode.Additive);
         }
     }
 
